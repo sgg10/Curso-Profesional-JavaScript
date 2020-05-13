cdf57822606f5dc3a39939f1a765f23ecf2f5369
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-05-13 07:12:08 -0500

    Se implementa el observer de cambio de tabs para el autopause

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -3,12 +3,16 @@ class AutoPause{
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
 
     run(player){
+        // *Autopause si el video tiene una visibilidad <= al 25% en la pantalla
         this.player = player
         const observer = new IntersectionObserver(this.handleIntersection, {threshold: this.threshold})// handler - config(umbral en porcentaje)
         observer.observe(this.player.media)
+        // *Autopause si el usuario cambia de pestaña
+        document.addEventListener('visibilitychange', this.handleVisibilityChange)
     }
 
     handleIntersection(entries){
@@ -16,6 +20,11 @@ class AutoPause{
         const entry = entries[0];
         entry.intersectionRatio >= this.threshold ? this.player.play() : this.player.pause()
         //console.log(entry)
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === "visible"
+        isVisible ? this.player.play() : this.player.pause()
     }
 }
 
